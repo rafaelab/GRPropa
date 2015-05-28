87144b8f081e4903dfb1dcb3b06a3f9cbad0a734
rafaelab <8rafael@gmail.com>
2015-05-28 11:53:21 +0100

    fix indentation

--- include/grpropa/ProgressBar.h
+++ include/grpropa/ProgressBar.h
@@ -10,26 +10,26 @@ namespace grpropa {
 
 class ProgressBar {
 private:
-	unsigned long _steps;
-	unsigned long _currentCount;
-	unsigned long _maxbarLength;
-	unsigned long _nextStep;
-	unsigned long _updateSteps;
-	time_t _startTime;
-	std::string stringTmpl;
-	std::string arrow;
+    unsigned long _steps;
+    unsigned long _currentCount;
+    unsigned long _maxbarLength;
+    unsigned long _nextStep;
+    unsigned long _updateSteps;
+    time_t _startTime;
+    std::string stringTmpl;
+    std::string arrow;
 
 public:
-	/// Initialize a ProgressBar with [steps] number of steps, updated at [updateSteps] intervalls
-	ProgressBar(unsigned long steps = 0, unsigned long updateSteps = 100);
-	void start(const std::string &title);
+    /// Initialize a ProgressBar with [steps] number of steps, updated at [updateSteps] intervalls
+    ProgressBar(unsigned long steps = 0, unsigned long updateSteps = 100);
+    void start(const std::string &title);
 
-	/// update the progressbar
-	/// should be called steps times in a loop 
-	void update();
+    /// update the progressbar
+    /// should be called steps times in a loop 
+    void update();
 
-	/// Mark the progressbar with an error
-	void setError();
+    /// Mark the progressbar with an error
+    void setError();
 };
 
 } // namespace grpropa
