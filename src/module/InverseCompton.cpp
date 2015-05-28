920bb103a140a78ca484ec0296d579709bb8e398
rafaelab <8rafael@gmail.com>
2015-05-28 12:16:47 +0100

    reading tables with redshift dependence for EBL models

--- src/module/InverseCompton.cpp
+++ src/module/InverseCompton.cpp
@@ -95,62 +95,169 @@ void InverseCompton::setThresholdEnergy(double Ethr) {
 
 void InverseCompton::initRate(std::string filename) {
     
-    std::ifstream infile(filename.c_str());
+    if (redshiftDependence == false) {
+        std::ifstream infile(filename.c_str());
+        if (!infile.good())
+            throw std::runtime_error("PairProduction: could not open file " + filename);
+   
+        // clear previously loaded interaction rates
+        tabEnergy.clear();
+        tabRate.clear();
 
-    if (!infile.good())
-        throw std::runtime_error("InverseCompton: could not open file " + filename);
-
-    // clear previously loaded interaction rates
-    tabEnergy.clear();
-    tabRate.clear();
+        while (infile.good()) {
+            if (infile.peek() != '#') {
+                double a, b;
+                infile >> a >> b;
+                if (infile) {
+                    tabEnergy.push_back(a * eV);
+                    tabRate.push_back(b / Mpc);
+                }
+            }
+            infile.ignore(std::numeric_limits < std::streamsize > ::max(), '\n');
+        }
+        infile.close();
+    } else { // Rates for EBL
+        std::ifstream infile(filename.c_str());
+        if (!infile.good())
+            throw std::runtime_error("PairProduction: could not open file " + filename);
   
-    // For now approximating ICS interaction rate as constant.
-    // Should be replaced by the correct values.
-    
-    /*int n = 50;
-    for (int i=0; i<n; i++){
-        double en = pow(10, 10+.1*i);
-        double t = 1.2e11;
-        tabEnergy.push_back(en * eV);
-        tabRate.push_back(1/(c_light*t));
-    }*/
-    
-    
-    while (infile.good()) {
-        if (infile.peek() != '#') {
-            double a, b;
-            infile >> a >> b;
-            if (infile) {
-                tabEnergy.push_back(a * eV);
-                tabRate.push_back(1 / (b * Mpc));
-                //std::cout << b << "  "  << a << std::endl;
+        // clear previously loaded interaction rates
+        tabEnergy.clear();
+        tabRate.clear();
+        tabRedshift.clear();
+
+        // size of vector is predefined and depends on the model
+        int nc; // number of columns (redshifts + one column for energy)
+        int nl = 81; // number of lines (energies)
+        std::vector<double> redshifts;
+        if (photonField == EBL_Finke10) {
+            nc = 33;
+            double redshifts[] = {0.00, 0.01, 0.02, 0.03, 0.04, 0.05, 0.07, 0.09, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 0.60, 0.70, 0.80, 0.90, 1.00, 1.20, 1.40, 1.60, 1.80, 2.00, 2.50, 3.00, 3.50, 4.00, 4.50, 4.99};
+            for (int k=0; k<nc; k++) 
+                tabRedshift.push_back(redshifts[k]);
+        }
+        else if (photonField == EBL_Gilmore12) {
+            nc = 20;
+            double redshifts[] = {0, 0.015, 0.025, 0.044, 0.05, 0.2, 0.4, 0.5, 0.6, 0.8, 1.0, 1.25, 1.5, 2.0, 2.5, 3.0, 4.0, 5.0, 6.0, 7.0};
+            for (int k=0; k<nc; k++) 
+                tabRedshift.push_back(redshifts[k]);
+        }
+        else if (photonField == EBL_Dominguez11 || photonField == EBL_Dominguez11_UL || photonField == EBL_Dominguez11_LL) {
+            nc = 20;
+            double redshifts[] = {0, 0.01, 0.03, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.8, 1.0, 1.25, 1.5, 2.0, 2.5, 3.0, 3.9};
+            for (int k=0; k<nc; k++) 
+                tabRedshift.push_back(redshifts[k]);
+        }
+        else if (photonField == EBL_Kneiske10) {
+            nc = 5;
+            double redshifts[] = {0.0, 0.1, 0.3, 0.8, 2.0};
+            for (int k=0; k<nc; k++) 
+                tabRedshift.push_back(redshifts[k]);
+        }
+        else if (photonField == EBL_Franceschini08) {
+            nc = 11;
+            double redshifts[] = {0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0};
+            for (int k=0; k<nc; k++) 
+                tabRedshift.push_back(redshifts[k]);
+        }
+        else { 
+            throw std::runtime_error("EBL model not defined for redshift dependent treatment (or not defined at all).");
+        }
+
+        double entries[nc+1][nl];
+        int j = 0;
+        while (!infile.eof()) {
+            for (int i=0; i<nc+1; i++) {
+                double entry = 0;
+                infile >> entry;
+                entries[i][j] = entry;
+            }
+            j++;
+        }
+
+        for (int j=0; j<nl; j++) 
+            tabEnergy.push_back(entries[0][j] * eV);
+        for (int i=1; i<nc+1; i++){
+            for (int j=0; j<nl; j++){
+                tabRate.push_back(entries[i][j] / Mpc);
             }
         }
-        infile.ignore(std::numeric_limits < std::streamsize > ::max(), '\n');
-    }
-    infile.close();
+        infile.close();
+    } // conditional: redshift dependent
+
+    // for (int i=0; i<tabRate.size(); i++) std::cout << tabRate[i] * Mpc << std::endl;
+    // for (int i=0; i<tabEnergy.size(); i++) std::cout << tabEnergy[i] /eV << std::endl;
     
 }
 
 void InverseCompton::initTableBackgroundEnergy(std::string filename) {
-    std::ifstream infile(filename.c_str());
-    if (!infile.good())
-        throw std::runtime_error("InverseCompton: could not open file " + filename);
-    tabPhotonEnergy.clear();
-    tabProb.clear();
+   if (redshiftDependence == false) {
+        std::ifstream infile(filename.c_str());
+        if (!infile.good())
+            throw std::runtime_error("PairProduction: could not open file " + filename);
+   
+        // clear previously loaded interaction rates
+        tabPhotonEnergy.clear();
+        tabProb.clear();
 
-    while (infile.good()) {
-        if (infile.peek() != '#') {
-            double a, b;
-            infile >> a >> b;
-            if (infile){
-                tabPhotonEnergy.push_back(b * eV);
-                tabProb.push_back(a);
+        while (infile.good()) {
+            if (infile.peek() != '#') {
+                double a, b;
+                infile >> b >> a;
+                if (infile) {
+                    tabPhotonEnergy.push_back(a * eV);
+                    tabProb.push_back(b);
+                }
+            }
+            infile.ignore(std::numeric_limits < std::streamsize > ::max(), '\n');
+        }
+        infile.close();
+    } else { // Rates for EBL
+        std::ifstream infile(filename.c_str());
+        if (!infile.good())
+            throw std::runtime_error("PairProduction: could not open file " + filename);
+  
+        // clear previously loaded interaction rates
+        tabPhotonEnergy.clear();
+        tabProb.clear();
+
+        // size of vector is predefined and depends on the model
+        int nc; // number of columns (redshifts + one column for energy)
+        int nl = 400; // number of lines (probabilities)
+
+        if (photonField == EBL_Finke10) nc = 33;
+        else if (photonField == EBL_Gilmore12) nc = 20;
+        else if (photonField == EBL_Kneiske10) nc = 5;
+        else if (photonField == EBL_Franceschini08) nc = 11;
+        else if (photonField == EBL_Dominguez11 || photonField == EBL_Dominguez11_UL || photonField == EBL_Dominguez11_LL) nc = 18;
+        else throw std::runtime_error("EBL model not defined for redshift dependent treatment (or not defined at all).");
+
+        double entries[nc+1][nl];
+        int j = 0;
+        while (!infile.eof()) {
+            for (int i=0; i<nc+1; i++) {
+                double entry = 0;
+                infile >> entry;
+                entries[i][j] = entry;
+            }
+            j++;
+        }
+
+        for (int j=0; j<nl; j++) 
+            tabProb.push_back(entries[0][j]);
+        for (int i=1; i<nc+1; i++){
+            for (int j=0; j<nl; j++){
+                tabPhotonEnergy.push_back(entries[i][j] * eV);
             }
         }
-        infile.ignore(std::numeric_limits < std::streamsize > ::max(), '\n');
-    }
-    infile.close();
+        infile.close();
+
+        // test
+        // for (int i=0; i<tabPhotonEnergy.size(); i++) std::cout << tabPhotonEnergy[i] / eV << std::endl;
+        // for (int i=0; i<tabProb.size(); i++) std::cout << tabProb[i] << std::endl;
+        // for (int i=0; i<tabRedshift.size(); i++) std::cout << tabRedshift[i] << std::endl;
+    } // conditional: redshift dependent
+}
 }
 
 double InverseCompton::energyFraction(double E, double z) const {
