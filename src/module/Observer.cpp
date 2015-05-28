29149d8f8e0ba0e055e79e4f5f43bde6c82c4899
rafaelab <8rafael@gmail.com>
2015-05-28 17:48:14 +0100

    modularized observers

--- src/module/Observer.cpp
+++ src/module/Observer.cpp
@@ -191,7 +191,7 @@ ObserverOutput3D::ObserverOutput3D(std::string fname) {
     description = "ObserverOutput3D: " + fname;
     fout.open(fname.c_str());
     fout
-            << "# D\tID\tID0\tE\tE0\tX\tY\tZ\tX0\tY0\tZ0\tPx\tPy\tPz\tP0x\tP0y\tP0z\tz\n";
+            << "# dT\tD\tID\tID0\tE\tE0\tX\tY\tZ\tX0\tY0\tZ0\tPx\tPy\tPz\tP0x\tP0y\tP0z\tz\n";
     fout << "#\n";
     fout << "# D           Trajectory length [Mpc]\n";
     fout << "# ID          Particle type (PDG MC numbering scheme)\n";
@@ -212,25 +212,23 @@ void ObserverOutput3D::onDetection(Candidate *candidate) const {
     char buffer[512];
     size_t p = 0;
 
-    p += sprintf(buffer + p, "%15.14e\t",
-            candidate->getTrajectoryLength() / Mpc);
+    Vector3d xi = candidate->source.getPosition();
+    Vector3d xf = candidate->current.getPosition();
+    double dt = (xf - xi).getR() / c_light;
+    p += sprintf(buffer + p, "%.9e\t", dt);
     p += sprintf(buffer + p, "%10i\t", candidate->current.getId());
     p += sprintf(buffer + p, "%10i\t", candidate->source.getId());
-    p += sprintf(buffer + p, "%4.4e\t",
-            candidate->current.getEnergy() / eV);
-    p += sprintf(buffer + p, "%4.4e\t",
-            candidate->source.getEnergy() / eV);
+    p += sprintf(buffer + p, "%4.4e\t", candidate->current.getEnergy() / eV);
+    p += sprintf(buffer + p, "%4.4e\t", candidate->source.getEnergy() / eV);
     Vector3d pos = candidate->current.getPosition() / Mpc;
-    p += sprintf(buffer + p, "%16.15e\t%16.15e\t%16.15e\t", pos.x, pos.y, pos.z);
+    p += sprintf(buffer + p, "%7.6e\t%7.6e\t%7.6e\t", pos.x, pos.y, pos.z);
     Vector3d ipos = candidate->source.getPosition() / Mpc;
-    p += sprintf(buffer + p, "%9.5e\t%9.5e\t%9.5e\t", ipos.x, ipos.y,
-            ipos.z);
+    p += sprintf(buffer + p, "%7.6e\t%7.6e\t%7.6e\t", ipos.x, ipos.y, ipos.z);
     Vector3d dir = candidate->current.getDirection();
-    p += sprintf(buffer + p, "%16.15f\t%16.15f\t%16.15f\t", dir.x, dir.y, dir.z);
+    p += sprintf(buffer + p, "%.8e\t%.8e\t%.8e\t", dir.x, dir.y, dir.z);
     Vector3d idir = candidate->source.getDirection();
-    p += sprintf(buffer + p, "%7.6f\t%7.6f\t%7.6f\t", idir.x, idir.y,
-            idir.z);
-    p += sprintf(buffer + p, "%10.9f\n", candidate->getRedshift());
+    p += sprintf(buffer + p, "%.8e\t%.8e\t%.8e\t", idir.x, idir.y, idir.z);
+    p += sprintf(buffer + p, "%.9e\n", candidate->getRedshift());
 
 
 #pragma omp critical
@@ -249,7 +247,7 @@ ObserverOutput1D::ObserverOutput1D(std::string fname) {
     fout << "# E   Energy [EeV]\n";
     fout << "# D   Comoving trajectory length [Mpc]\n";
     fout << "# ID0 Initial particle type\n";
-    fout << "# E0  Initial energy [EeV]\n";
+    fout << "# E0  Initial energy [eV]\n";
 }
 
 ObserverOutput1D::~ObserverOutput1D() {
@@ -261,13 +259,10 @@ void ObserverOutput1D::onDetection(Candidate *candidate) const {
     size_t p = 0;
 
     p += sprintf(buffer + p, "%10i\t", candidate->current.getId());
-    p += sprintf(buffer + p, "%8.4f\t",
-            candidate->current.getEnergy() / EeV);
-    p += sprintf(buffer + p, "%9.4f\t",
-            candidate->getTrajectoryLength() / Mpc);
+    p += sprintf(buffer + p, "%.4e\t", candidate->current.getEnergy() / eV);
+    p += sprintf(buffer + p, "%9.4f\t", candidate->getTrajectoryLength() / Mpc);
     p += sprintf(buffer + p, "%10i\t", candidate->source.getId());
-    p += sprintf(buffer + p, "%8.4f\n",
-            candidate->source.getEnergy() / EeV);
+    p += sprintf(buffer + p, "%.4e\n", candidate->source.getEnergy() / eV);
 
 #pragma omp critical
     {
@@ -276,170 +271,4 @@ void ObserverOutput1D::onDetection(Candidate *candidate) const {
     }
 }
 
-// ////////////////////////////////////////////////////////////////////////////////
-
-// SmallObserverSphere::SmallObserverSphere(Vector3d center, double radius,
-//      std::string flag, std::string flagValue, bool makeInactive) :
-//      center(center), radius(radius), flag(flag), flagValue(flagValue), makeInactive(
-//              makeInactive), maximumTrajectory(
-//              std::numeric_limits<double>::max()) {
-// }
-
-// void SmallObserverSphere::process(Candidate *candidate) const {
-//  // current distance to observer sphere center
-//  double d = (candidate->current.getPosition() - center).getR();
-//  double remainingToBorder = d - radius;
-
-//  // conservatively limit next step to prevent overshooting
-//  candidate->limitNextStep(fabs(remainingToBorder));
-
-//  // no detection if outside of observer sphere
-//  if (d > radius) {
-
-//      // disable candidate if it cannot reach this observer
-//      double remainingToMaximum = maximumTrajectory
-//              - candidate->getTrajectoryLength();
-//      if (remainingToBorder > remainingToMaximum)
-//          candidate->setActive(false);
-
-//      return;
-//  }
-
-//  // previous distance to observer sphere center
-//  double dprev = (candidate->previous.getPosition() - center).getR();
-
-//  // if particle was inside of sphere in previous step it has already been detected
-//  if (dprev <= radius)
-//      return;
-
-//  // else: detection
-//  candidate->setProperty(flag, flagValue);
-//  if (makeInactive)
-//      candidate->setActive(false);
-// }
-
-// void SmallObserverSphere::setCenter(Vector3d c) {
-//  center = c;
-// }
-
-// void SmallObserverSphere::setRadius(double r) {
-//  radius = r;
-// }
-
-// void SmallObserverSphere::setFlag(std::string f, std::string v) {
-//  flag = f;
-//  flagValue = v;
-// }
-
-// void SmallObserverSphere::setMakeInactive(bool b) {
-//  makeInactive = b;
-// }
-
-// void SmallObserverSphere::setMaximumTrajectory(double maximumTrajectory) {
-//  this->maximumTrajectory = maximumTrajectory;
-// }
-
-// std::string SmallObserverSphere::getDescription() const {
-//  std::stringstream s;
-//  s << "Small observer sphere: " << radius / Mpc;
-//  s << " Mpc radius around " << center / Mpc;
-//  s << " Mpc, Flag: '" << flag << "' -> '" << flagValue << "'";
-//  if (maximumTrajectory != std::numeric_limits<double>::max())
-//      s << ", Maximum trajectory: " << maximumTrajectory / Mpc << "Mpc";
-//  if (makeInactive)
-//      s << ", render inactivate";
-//  return s.str();
-// }
-
-// LargeObserverSphere::LargeObserverSphere(Vector3d center, double radius,
-//      std::string flag, std::string flagValue, bool makeInactive) :
-//      center(center), radius(radius), flag(flag), flagValue(flagValue), makeInactive(
-//              makeInactive) {
-// }
-
-// void LargeObserverSphere::process(Candidate *candidate) const {
-//  // current distance to observer sphere center
-//  double d = (candidate->current.getPosition() - center).getR();
-
-//  // conservatively limit next step size to prevent overshooting
-//  candidate->limitNextStep(fabs(radius - d));
-
-//  // no detection if inside observer sphere
-//  if (d < radius)
-//      return;
-
-//  // previous distance to observer sphere center
-//  double dprev = (candidate->previous.getPosition() - center).getR();
-
-//  // if particle was outside of sphere in previous step it has already been detected
-//  if (dprev >= radius)
-//      return;
-
-//  // else: detection
-//  candidate->setProperty(flag, flagValue);
-//  if (makeInactive)
-//      candidate->setActive(false);
-// }
-
-// void LargeObserverSphere::setCenter(Vector3d c) {
-//  center = c;
-// }
-
-// void LargeObserverSphere::setRadius(double r) {
-//  radius = r;
-// }
-
-// void LargeObserverSphere::setFlag(std::string f, std::string v) {
-//  flag = f;
-//  flagValue = v;
-// }
-
-// void LargeObserverSphere::setMakeInactive(bool b) {
-//  makeInactive = b;
-// }
-
-// std::string LargeObserverSphere::getDescription() const {
-//  std::stringstream s;
-//  s << "Large observer sphere: " << radius / Mpc;
-//  s << " Mpc radius around " << center / Mpc;
-//  s << " Mpc, Flag: '" << flag << "' -> '" << flagValue << "'";
-//  if (makeInactive)
-//      s << ", render inactivates";
-//  return s.str();
-// }
-
-// Observer1D::Observer1D() {
-//  setDescription("1D observer");
-// }
-
-// void Observer1D::process(Candidate *candidate) const {
-//  double x = candidate->current.getPosition().x;
-//  // check if position x > 0
-//  if (x > std::numeric_limits<double>::min()) {
-//      candidate->limitNextStep(x);
-//      return;
-//  }
-//  // else: detection
-//  candidate->setProperty("Detected", "");
-//  candidate->setActive(false);
-// }
-
-// DetectAll::DetectAll(std::string f, std::string v, bool m) :
-//      flag(f), flagValue(v), makeInactive(m) {
-// }
-
-// void DetectAll::process(Candidate *candidate) const {
-//  candidate->setProperty(flag, flagValue);
-//  if (makeInactive)
-//      candidate->setActive(false);
-// }
-
-// std::string DetectAll::getDescription() const {
-//  std::stringstream s;
-//  s << "DetectAll: Flag: " << flag << " -> " << flagValue;
-//  if (makeInactive)
-//      s << ", render inactive";
-//  return s.str();
-// }
-
-} // namespace grpropa
+}// namespace
