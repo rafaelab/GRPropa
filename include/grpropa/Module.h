34a608812d341ffe63b13e44a47415df4ad8ee38
Rafael Alves Batista <8rafael@gmail.com>
2016-02-17 11:50:49 +0000

    refactor modules to follow CRPropa

--- include/grpropa/Module.h
+++ include/grpropa/Module.h
@@ -23,12 +23,47 @@ public:
     }
     virtual std::string getDescription() const;
     void setDescription(const std::string &description);
+    virtual void beginRun();
+    virtual void endRun();
     virtual void process(Candidate *candidate) const = 0;
     inline void process(ref_ptr<Candidate> candidate) const {
         process(candidate.get());
     }
 };
 
+
+/**
+ @class AbstractCondition
+ @brief Abstract Module providing common features for conditional modules.
+ */
+class AbstractCondition: public Module {
+protected:
+    ref_ptr<Module> rejectAction, acceptAction;
+    bool makeRejectedInactive, makeAcceptedInactive;
+    std::string rejectFlagKey, rejectFlagValue;
+    std::string acceptFlagKey, acceptFlagValue;
+
+    void reject(Candidate *candidate) const;
+    inline void reject(ref_ptr<Candidate> candidate) const {
+        reject(candidate.get());
+    }
+
+    void accept(Candidate *candidate) const;
+    inline void accept(ref_ptr<Candidate> candidate) const {
+        accept(candidate.get());
+    }
+
+public:
+    AbstractCondition();
+    void onReject(Module *rejectAction);
+    void onAccept(Module *acceptAction);
+    void setMakeRejectedInactive(bool makeInactive);
+    void setMakeAcceptedInactive(bool makeInactive);
+    void setRejectFlag(std::string key, std::string value);
+    void setAcceptFlag(std::string key, std::string value);
+    void beginRun();
+    void endRun();
+};
 } // namespace grpropa
 
 #endif /* GRPROPA_MODULE_H */
