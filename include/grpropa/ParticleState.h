5adb3cfdfe65a4ff15260e6ee5764a15a94eaded
Rafael Alves Batista <8rafael@gmail.com>
2017-02-01 12:58:43 +0000

    speed from Lorentz factor

--- include/grpropa/ParticleState.h
+++ include/grpropa/ParticleState.h
@@ -19,34 +19,34 @@ namespace grpropa {
  */
 class ParticleState {
 private:
-    int id; /**< particle ID (Particle Data Group numbering scheme) */
-    double energy; /**< total energy */
-    Vector3d position; /**< position vector in comoving coordinates */ 
-    Vector3d direction; /**< unit vector of velocity or momentum */
-    double pmass; /**< particle rest mass */ 
-    double charge; /**< particle charge */
+    int id; /* particle ID (Particle Data Group numbering scheme) */
+    double energy; /* total energy */
+    Vector3d position; /* position vector in comoving coordinates */ 
+    Vector3d direction; /* unit vector of velocity or momentum */
+    double pmass; /* particle rest mass */ 
+    double charge; /* particle charge */
 
 public:
     ParticleState(int id = 0, double energy = 0, Vector3d position = Vector3d(0, 0, 0), Vector3d direction = Vector3d(-1, 0, 0));
 
     std::string getDescription() const;
-    void setPosition(const Vector3d &pos); //**< Set position in comoving coordinates */
-    const Vector3d &getPosition() const; /**< Get position in comoving coordinates */
-    void setDirection(const Vector3d &dir); /**< Set direction unit vector, non unit-vectors are normalized */
-    const Vector3d &getDirection() const; /**< Get direction unit vector */
-    void setEnergy(double newEnergy); /**< Set energy in [J] */
-    double getEnergy() const; /**< Get energy in [J] */
-    void setId(int); /**< Set particle ID */
-    int getId() const; /**< Get particle ID */
+    void setPosition(const Vector3d &pos); //* Set position in comoving coordinates */
+    const Vector3d &getPosition() const; /* Get position in comoving coordinates */
+    void setDirection(const Vector3d &dir); /* Set direction unit vector, non unit-vectors are normalized */
+    const Vector3d &getDirection() const; /* Get direction unit vector */
+    void setEnergy(double newEnergy); /* Set energy in [J] */
+    double getEnergy() const; /* Get energy in [J] */
+    void setId(int); /* Set particle ID */
+    int getId() const; /* Get particle ID */
 
 
-    double getCharge() const; /**< Electric charge of the particle in [C] */
-    double getMass() const; /**< Mass of the particle in [kg] */
-    void setLorentzFactor(double gamma); /**< Set Lorentz factor and modify the particle's energy accordingly */
-    double getLorentzFactor() const; /**< Get Lorentz factor */
-    Vector3d getVelocity() const; /**< Velocity: direction times the speed of light in [m/s] */
-    Vector3d getMomentum() const; /**< Momentum: direction times energy divided by the speed of light [kg m/s] */
-    double getSpeed() const; /**< Returns the speed [m/s] */
+    double getCharge() const; /* Electric charge of the particle in [C] */
+    double getMass() const; /* Mass of the particle in [kg] */
+    void setLorentzFactor(double gamma); /* Set Lorentz factor and modify the particle's energy accordingly */
+    double getLorentzFactor() const; /* Get Lorentz factor */
+    Vector3d getVelocity() const; /* Velocity: direction times the speed of light in [m/s] */
+    Vector3d getMomentum() const; /* Momentum: direction times energy divided by the speed of light [kg m/s] */
+    double getSpeed() const; /* Returns the speed [m/s] */
 };
 
 } // namespace grpropa
