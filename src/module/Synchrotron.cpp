4efa2a0cfe7016c0af2adeb747af6e7586e1c3e3
Rafael Alves Batista <8rafael@gmail.com>
2017-02-22 15:50:27 +0000

    fixed problem of increasingly high energies

--- src/module/Synchrotron.cpp
+++ src/module/Synchrotron.cpp
@@ -20,22 +20,27 @@ void Synchrotron::process(Candidate *c) const {
 
     double z = c->getRedshift();
     double E = c->current.getEnergy() * (1 + z);
-    double lf = c->current.getLorentzFactor();
+    double lf = c->current.getLorentzFactor() * (1 + z);
     Vector3d pos = c->current.getPosition();
     Vector3d b = Bfield->getField(pos);
     Vector3d v = c->current.getVelocity();
     double beta = c->current.getSpeed() / c_light;
     double step = c->getCurrentStep() / (1 + z);
-    double B = b.getR();
+    double B = b.getR() * pow(1 + z, 2);
 
     double vperp = v.cross(b).getR() / B;
     double RL = lf * mass_electron * vperp / (eplus * B);
     double dEdx = (2. / 3.) * pow(eplus, 2) * pow(beta * lf, 4) / pow(RL, 2); 
 
     double dE = std::abs(dEdx * step);
-    dE = std::min(E, dE); 
-    c->current.setEnergy(E - dE);
-    // c->limitNextStep(limit * E / dEdx);
+    dE = std::min(E, dE);
+    double Enew = (E - dE) / (1 + z);
+
+    if (Enew * (1 + z) > E)
+        return;
+
+    c->current.setEnergy(Enew);
+    c->limitNextStep(limit * E / dEdx);
 }
 
 
